f98fbb9e0e111c618e5e9b0e5cf19b77e4d3f530
crisanalex15 <alex.crisan@student.upt.ro>
2025-03-20 13:34:21 +0200

    Proiect terminat

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -8,9 +8,56 @@ namespace SpendSmart.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SpendSmartDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Expenses()
+        {
+            var allExpenses = _context.Expenses.ToList();
+            var totalExpenses = allExpenses.Sum(e => e.Value);
+            ViewBag.Expenses = totalExpenses;
+            return View(allExpenses);
+        }
+
+        public IActionResult CreateEditExpense(int? id)
+        {
+            if(id != null)
+            {
+                var expenseInDb = _context.Expenses.FirstOrDefault(e => e.Id == id);
+                return View(expenseInDb);
+            }
+            return View();
+        }
+        public IActionResult DeleteExpense(int id)
+        {
+            var expenseinDb = _context.Expenses.Find(id);
+            //var expenseInDb = _context.Expenses.FirstOrDefault(e => e.Id == id); // folosim doar daca avem conditii suplimentare 
+
+            _context.Expenses.Remove(expenseinDb);
+            _context.SaveChanges();
+
+            return RedirectToAction("Expenses");
+        }
+
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            if(model.Id==0)
+            {
+                //create
+            _context.Expenses.Add(model);
+            }
+            else
+            {
+                //edit
+                _context.Expenses.Update(model);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Expenses");
         }
 
         public IActionResult Index()
---
+++ SpendSmart/Models/SpendSmartDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; } // creeaza db pentru expenses
+
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
